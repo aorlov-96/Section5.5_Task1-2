13426950f246ff2bb655ed174fac45a0f15632c7
aorlov-96 <rtolyaorlov@yandex.ru>
2022-06-19 16:38:25 +0300

    Добавьте файлы проекта.

---
+++ Section5.5_Task1,2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            static string[] WordSentenceSeparator(string text) // метод деления предложения на слова.
+            {
+                char[] separators = new char[] { ' ', '.' };
+                string[] separatorWords = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                OutputtingWordsSeparation(separatorWords);
+                return separatorWords;
+            }
+            static string[] OutputtingWordsSeparation(string[] words) // метод вывода полученого массива
+            {
+                Console.WriteLine("Полученный массив:");
+                for (int i = 0; i < words.Length; i++)
+                {
+                    Console.WriteLine(words[i]);
+                }
+                return words;
+
+            }
+
+
+            Console.WriteLine("Введите предложение");
+            string sentence = Console.ReadLine();
+            WordSentenceSeparator(sentence); 
+
+
+
+
+        }
+
+    }
+
+
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace task1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            static string ReversWords(string inputPhrase) // метод переворота текста наооборот
+            {
+                string[] textResult = WordSentenceSeparator(inputPhrase);// делю предложение на слова
+                Array.Reverse(textResult); // переворачиваю массив, получаю слова в обратном порядке
+                string newString; 
+                string resultString = textResult[0] + " "; // присваиваю переменной первое слова перевернутого массива + пробел
+                for (int i = 1; i < textResult.Length; i++) // цикл сложения слов в предложение
+                {
+                    newString = textResult[i]; // присваиваю переменной слово из массива 
+                    resultString += newString + " "; // собираю предложение + пробел
+                }
+                
+                return resultString;
+
+            }
+
+
+            static string[] WordSentenceSeparator(string text) // метод деления предложения на слова.
+            {
+                char[] separators = new char[] { ' ', '.' }; // символы для исключения 
+                string[] separatorWords = text.Split(separators, StringSplitOptions.RemoveEmptyEntries); //собираю массив слов
+                return separatorWords;
+            }
+
+            Console.WriteLine("Введите предложение");
+            string sentence = Console.ReadLine();
+            sentence = ReversWords(sentence);
+            Console.WriteLine(sentence);
+            
+            
+
+
+
+
+        }
+    }
+}
+
+
+
+
